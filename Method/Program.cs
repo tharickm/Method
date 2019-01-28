00df814c6b5ae4fe68522cfacd29c36f3a2b45cc
tharick <tharick@mail.usf.edu>
2019-01-28 18:46:26 -0500

    Methods

--- Method/Program.cs
+++ Method/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿/*
+    Author: Tharick Moulton
+    Date: 1/28/2019
+    Comments: This C# Console application code demonstrates the use of
+              methods after getting input from a user. 
+*/
+
+
+
+
+
+
+using System;
 
 namespace Method
 {
@@ -6,7 +18,20 @@ namespace Method
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+            Console.WriteLine("Please enter your name");
+            // This variable will gather data from user input
+
+            string Username = Console.ReadLine();
+           // Stores user input as sting called "Username"
+            
+           
+            Console.WriteLine("Hello " + Username + "!");
+            // Displays users input back to screen
+            Console.WriteLine("Press any key to exit the program and try again ...");
+            Console.ReadKey(true);
+
+        }//End of main
+
+    }// End of class
+
+}// End of namespace
